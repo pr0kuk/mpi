72e6a8195a133f961761085ac1a560a802433c13
pr0kuk <shcherbakov.aa@phystech.edu>
2022-03-15 20:10:22 +0300

    added parallel division

--- exp/exp.c
+++ exp/exp.c
@@ -35,7 +35,7 @@ long int calc_n(long int p)
         for (;n >= 100; l++)
             n /= 10;
     }
-    return i;
+    return i + 2;
 }
 
 
@@ -47,28 +47,30 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    int my_rank = 0, commsize = 0, i = 0;
-    double tstart = 0, tfinish = 0, t1 = 0, t2 = 0, t3 = 0;
+    int my_rank = 0, commsize = 0, i = 0, j = 0;
+    double tstart = 0, tfinish = 0, tcalcs = 0, tcalcf = 0, tgather = 0;
     if (my_rank == 0)
         tstart = MPI_Wtime();
     long int p = atol(argv[1]), n = 0;
-
-    //char Result[MAXCHAR * 10];
-    char * Result = (char*)malloc(MAXCHAR * 4), *sumch_k = (char*)malloc(MAXCHAR);
-    big_int* c_k = dcreate("1"), * sum_k = dcreate("0");
-
+    big_int* c_k = dcreate("1"), * sum_k = dcreate("0"), * dnmr = dcreate("1"), * nmr, * rem, * chast, * des = dcreate("10");
+    char Result[MAXCHAR * 10], part_ans[MAXCHAR];
+    char * ans = (char*)malloc(MAXCHAR), *sumch_k = (char*)malloc(MAXCHAR);
+    for (i = 0; i < MAXCHAR; i++)
+        part_ans[i] = 0;
+    tstart = MPI_Wtime();
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
     
     if (my_rank == 0) {
-        t3 = MPI_Wtime();
+        for(i = 0; i < MAXCHAR * 5; i++)
+            Result[i] = 0;
         n = calc_n(p);
-        t1 = MPI_Wtime();
-        printf("\nn is %ld\n", n);
     }
 
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    tcalcs = MPI_Wtime();
+//COMPUTING ROW MEMBERS
     for (i = my_rank + 1; i <= n; i += commsize) {
         c_k = dcreate("1");
         fact(c_k, i, n);
@@ -76,39 +78,47 @@ int main(int argc, char *argv[])
         sumch_k = dprint(sum_k);
     }
 
-    //printf("summs %s %d\n", sumch_k, my_rank);
-    //printf("strlen %ld\n", strlen(sumch_k));
-    MPI_Gather(sumch_k, MAXCHAR, MPI_CHAR, Result, MAXCHAR, MPI_CHAR, 0, MPI_COMM_WORLD);
+//DIVISION
+    fact(dnmr, 1, n);
+    nmr = dcreate(sumch_k);
+    for (i = 0; i < p + 3; i++) {
+        chast = division(nmr, dnmr, &rem);
+        nmr = rem;  
+        multiply(nmr, des);
+        part_ans[i] = dprint(chast)[0];
+    }
+    tcalcf = MPI_Wtime();
+
+//GATHERING & SUMMING
+    MPI_Gather(part_ans, MAXCHAR, MPI_CHAR, Result, MAXCHAR, MPI_CHAR, 0, MPI_COMM_WORLD);
     if (my_rank == 0) {
-        t2 = MPI_Wtime();
-        big_int * summa0 = dcreate(Result), * dnmr = dcreate("1"), * nmr = summa0, *rem, * chast, * des = dcreate("10");
-        char ans[MAXCHAR];
-        for (i = 1; i < commsize; i++)
-            add(summa0, dcreate(Result + MAXCHAR * i));
-        double tfacts = MPI_Wtime();
-        fact(dnmr, 1, n);
-        double tfacte = MPI_Wtime();
-        printf("strlen dnmr %ld\n", strlen(dprint(dnmr)));
-        for (i = 0; i < p + 2; i++) {
-            if (i == 1)
-                ans[i++] = '.';
-            chast = division(nmr, dnmr, &rem);
-            nmr = rem;  
-            multiply(nmr, des);
-            ans[i] = dprint(chast)[0];
+        tgather = MPI_Wtime();
+        big_int * summa0 = dcreate("0");
+        for (i = 0; i < commsize; i++) {
+            for (j = 0;; j++) {
+                if ((Result+MAXCHAR*i+j)[0] != '0') {
+                    add(summa0, dcreate(Result + MAXCHAR * i + j)); 
+                    break; 
+                }
+            }
         }
         tfinish = MPI_Wtime();
-        //printf("tfact %f\n", -tfacts + tfacte); 
-        printf("calc_n %f\n", t1 - t3);  
-        printf("tmain %f\n", t2 - t1);  
-        printf("tdivision %f\n", tfinish - t2);
-        printf("tgeneral %f\n", tfinish - t3);
-        for (i = 0; i < p + 2; i++)
-            printf("%c", ans[i]);
+//OUTPUT
+        //ans = dprint(summa0);
+        //printf("\n");
+        //for (i = 0; i < p + 2; i++) {
+        //    if (i == 1)
+        //        printf(".");
+        //    printf("%c", ans[i]);
+        //}
+        //printf("\n");
         printf("\n");
+        printf("tgeneral %lf\n", tfinish - tstart);
+        printf("tmain %lf\n", tcalcf - tcalcs);
     }
+
+//ENDING
     free(sumch_k);
-    free(Result);
     MPI_Finalize();
     return 0;
 }
