a9cc0dfed7b3656dd68e11f3b907c5f4b3262e1b
pr0kuk <shcherbakov.aa@phystech.edu>
2022-03-30 14:09:16 +0300

    removed extra j variable

--- intro_tasks/mpiput.c
+++ intro_tasks/mpiput.c
@@ -1,4 +1,4 @@
-#define MAX_PROC 6
+#define MAX_PROC 6      
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpi.h"
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
         printf("n is not defined\n");
         exit(-1);
     }
-    int my_rank = 0, commsize = 0, i = 0, j = 0, n = atoi(argv[1]);
+    int my_rank = 0, commsize = 0, i = 0, n = atoi(argv[1]);
     long double summ = 0, ans = 0, Result[MAX_PROC];
+    MPI_Win window;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
-    MPI_Win window;
     MPI_Win_create(&Result, sizeof(long double)*MAX_PROC, sizeof(long double), MPI_INFO_NULL, MPI_COMM_WORLD, &window);
     MPI_Win_fence(0, window);
 
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     MPI_Win_fence(0, window);
 
     if (!my_rank) {
-        for(j = 0; j < commsize; ans += Result[j++]);
+        for(i = 0; i < commsize; ans += Result[i++]);
         printf("\nanswer is %Lf\n", ans);
     }
     MPI_Finalize();
