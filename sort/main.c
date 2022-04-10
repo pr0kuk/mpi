b957f1a3d4984919b07ff54c3dc5fb6038407e9e
pr0kuk <alex-shch@yandex.ru>
2022-04-10 21:46:53 +0300

    working sort on 1 thread

--- sort/main.c
+++ sort/main.c
@@ -1,64 +1,70 @@
 #define MAX_DIGITS 6
+#define SWAP(arr, i, l) do{c = *(arr+i), *(arr+i) = *(arr+l), *(arr+l) = c;}while(0)
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+//#include "mpi.h"
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 
-/*int get_n(int fd)
+int print_arr(int* arr, int n)
 {
-    int n = 0;
-    char buf[1];
-    while(read(fd, buf, 1) > 0)
-        if (buf[0] == ' ')
-            n++;
-    printf("n is %d\n", n);
-    return n;
-}
-
-int process_in(int* sizes, int fd, int n)
-{
-    char buf[1], buf2[MAX_DIGITS];
-    int i = 0, j = 0;
-    memset(buf2, 0, MAX_DIGITS);
-    lseek(fd, 0, SEEK_SET);
-    while(read(fd, buf, 1) > 0) {
-        if (buf[0] == '\n')
-            break;
-        if (buf[0] == ' ') {
-            sizes[i++] = atoi(buf2);
-            memset(buf2, 0, MAX_DIGITS);
-            j = 0;
-        }
-        else
-            buf2[j++] = buf[0];
-    }
-    return 0;
-}
-
-int main()
-{
-    int fd = open("in.txt", 0);
-    int* sizes;
-    int n = get_n(fd), i = 0, sum = 0;
-    sizes = (int*)malloc(n*sizeof(int));
-    process_in(sizes, fd, n);
-    for (i = 0; i < n; i++) {
-        printf("%d ", sizes[i]);
-        sum += sizes[i];
-    }
+    int i = 0;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     printf("\n");
-    int** mas = (int**)malloc(sum * sizeof(int));
-}*/
+}
 
 int main(int argc, char* argv[])
 {
-    int n = argv[1], i = 0, n2 = 0;
-    int* mas = (int*)malloc(n*sizeof(int));
-    for (i = 0; i < n; i++)
-        mas[i] = i;
-    n2 = n / 2 + 1;
-    int* mas1 = (int*)malloc(n2*sizeof(int)), mas2 = (int*)malloc(n2*sizeof(int));
+    int n = atoi(argv[1]), i = 0, n2 = 0, j =0, k =0, c = 0, l = 0, d = 0;
+    int my_rank, commsize;
+    int* marr = (int*)malloc((n+1)*sizeof(int));
+    for (i = 0; i < n; i++) {
+        marr[i] = n - i;
+    }
+    print_arr(marr, n);
+    //MPI_Init(&argc, &argv);
+    //MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    //MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    commsize = 4;
+    for (my_rank = 0; my_rank < commsize; my_rank++) {
+        printf("rank is %d\n", my_rank);
+        int np = n / commsize;
+        if (my_rank == commsize)
+            np+=n%commsize;
+        int* narr = marr + my_rank*np;
+        for (k = 2; k <= np; k *= 2) {
+            for (j=k>>1; j>0; j=j>>1) {
+                for (i = 0; i < np; i++) {
+                    l = i^j;
+                    if (l > i) {
+                        if ((i&k) == 0 && narr[i] > narr[l])
+                            SWAP(narr, i, l);
+                        if ((i&k) != 0 && narr[i] < narr[l])
+                            SWAP(narr, i, l);
+                    }
+                }
+            }
+        }
+    }
+    print_arr(marr, n);
+    //reduce
+    for (k = k /2; k <= n; k *= 2) {
+        for (j=k>>1; j>0; j=j>>1) {
+            for (i = 0; i < n; i++) {
+                l = i^j;
+                if (l > i) {
+                    if ((i&k) == 0 && marr[i]> marr[l])
+                        SWAP(marr, i, l);   
+                    if ((i&k) != 0 && marr[i] < marr[l])
+                        SWAP(marr, i, l);
+                }
+            }
+        }
+    }
+    print_arr(marr, n);
+    //MPI_FINALIZE();
 }
